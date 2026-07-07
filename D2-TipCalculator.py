9d6c69d8e23a18bf04ecf31ba46839d04fc852b9
LuiyTheNinja <jc46124@gmail.com>
2026-07-07 00:26:34 -0400

    Day 2 Project: Tip Calculator

--- D2-TipCalculator.py
+++ D2-TipCalculator.py
@@ -1,2 +1,11 @@
 # Day 2 Project: Tip Calculator
 
+bill_total = float(input("What was the total bill? $"))
+tip_percentage = int(input("What percentage tip would you like to give? 10, 12, 15, or 20? "))
+num_people = int(input("How many people to split the bill? "))
+
+tip_amount = bill_total * (tip_percentage / 100)
+total_bill = bill_total + tip_amount
+bill_per_person = total_bill / num_people
+
+print(f"Each person should pay: ${bill_per_person:.2f}")
