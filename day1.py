4c2b3d508675848e689bc173ff2f35f52cb294f3
LuiyTheNinja <jc46124@gmail.com>
2026-07-06 23:50:04 -0400

    Day 1 Project: Band Name Generator

---
+++ day1.py
@@ -0,0 +1,7 @@
+# Day 1 Project: Band Name Generator
+
+name = input("What is your name?")
+location = input("Where are you from?")
+animal = input("What is your favorite animal?")
+
+print(f"Your band name is {name}, the {animal} from {location}.")
