37644b5fb9e83ffe86ba1d6d36c21506e6566469
LuiyTheNinja <68018060+LuiyTheNinja@users.noreply.github.com>
2026-07-09 10:00:21 -0400

    Add D4-Heads_or_Tails activity, And added to D4 RockPaperScissors project.

---
+++ D4-Heads-or-Tails
@@ -0,0 +1,8 @@
+import random
+
+heads_tails = random.randint(0, 1)
+
+if heads_tails == 0:
+    print("Heads")
+if heads_tails == 1:
+    print("Tails")
--- D4-RockPaperScissors.py
+++ D4-RockPaperScissors.py
@@ -1,3 +1,28 @@
+import random
+# Python uses the Mersenne Twister. 
+# we use the import random for this. 
+# in the documentation for this, we can all the things this does. 
+# here we will use randint. 
+
+# random_integer = random.randint(1 , 10)
+# print(random_integer)
+
+# We can also split code up into individual modules, which is what is done above. 
+# to import into another py file. We do
+# "import filename"
+# to then call specific functions or variables, we need to use full stop with the file name
+# ex: print(my_module.my_favourite_number)
+# random.random() will generate a foat between 0 and 1. 
+# We can multiple the random.random to increase the random range.
+# ex: random.random() * 10 will give us floats between 0 and 10. 
+
+# random.uniform(a, b) will generate numbers between and inclusie of a and b. Favors upper bound
+# preferable to use random.random() * <whatever number is needed>
+
+# ==================================================================
+
+# Code for Player input
+
 user_input = input("What do you choose? Type 0 for Rock, Type 1 for Paper or Type 2 for Scissors.\n")
 
 # Temporary code to display the user's choice
@@ -7,6 +32,26 @@ elif user_input == "1":
     print("You chose Paper.")
 else:
     print("You chose Scissors.")
-# Temporary code to display the user's choice
+# =========================================
 
-# TODO: Write code to generate a random choice for the computer | Include ASCI Art for player and computer choices. | Determine winner 
+# Function to generate a random computer choice. 
+computer_choice = random.randint(0, 2)
+
+def computer_choice():
+    if computer_choice == 0:
+        print("Computer chose Rock!")
+    elif computer_choice == 1:
+        print("Computer chose Paper!")
+    else: 
+        print("Computer chose Scissors!")
+
+# ==============================================
+
+# TODO: Include ASCI Art for player and computer choices. | Determine winner 
+
+def result():
+    if user_input == 0 and computer_choice == 0 or user_input == 1 and computer_choice == 1 or user_input == 2 and computer_choice == 2:
+        print("Tie! Try Again!")
+    if user_input == 1 and computer_choice == 1:
+        print("Computer wins!")
+
