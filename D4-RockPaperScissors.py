1d9f01733061e7b2da8d3b6958933d1da1e901f3
LuiyTheNinja <jc46124@gmail.com>
2026-07-09 01:34:00 -0400

    Day 4-RockPaperScissors Project initial commit.

--- D4-RockPaperScissors.py
+++ D4-RockPaperScissors.py
@@ -1,1 +1,10 @@
 user_input = input("What do you choose? Type 0 for Rock, Type 1 for Paper or Type 2 for Scissors.\n")
+
+# Temporary code to display the user's choice
+if user_input == "0":
+    print("You chose Rock.")
+elif user_input == "1":
+    print("You chose Paper.")
+else:
+    print("You chose Scissors.")
+# Temporary code to display the user's choice
