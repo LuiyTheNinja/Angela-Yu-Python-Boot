621f8d596870c9ae03e7f5856d16b8368aa062cb
LuiyTheNinja <jc46124@gmail.com>
2026-07-16 01:12:51 -0400

    Added starting variables for Day8-Ceaser-Cypher project

---
+++ Day 8/Caesar_Cipher_1/main.py
@@ -0,0 +1,7 @@
+encrypt_or_decode = input("Type 'encrypt' to encrypt, type 'decode' to decrypt:\n")
+message = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+encode_result = ""
+
+printf("The encoded result is: " + encode_result)
