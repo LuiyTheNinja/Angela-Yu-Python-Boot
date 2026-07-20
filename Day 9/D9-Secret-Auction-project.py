3daa9c87b33ffd11e8d4cd9d1bf632a445eef18b
LuiyTheNinja <jc46124@gmail.com>
2026-07-20 01:50:40 -0400

    Started Day9 Project - Secret-Auction

--- Day 9/D9-Secret-Auction-project.py
+++ Day 9/D9-Secret-Auction-project.py
@@ -3,9 +3,21 @@ import art
 print(art.logo)
 
 
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+# TODO-1: Ask the user for input = DONE
+# TODO-2: Save data into dictionary {name: price} = DONE
+# TODO-3: Whether if new bids need to be added = DONE
+# TODO-4: Compare bids in dictionary 
+
+
+print("Welcome to the secret auction program.")
+should_continue = input("Do you want to make a bid? Type 'yes' or 'no': ")
+
+while should_continue == "yes":
+  name = input("What is your name?: ")
+  bid = int(input("What is your bid?: $"))
+  bids = {name: bid} # how to add new bids to the dictionary without overwriting the previous ones?
+  should_continue = input("Is there another Bidder? Type 'yes' or 'no': ")
+
+print(bids)
 
 
