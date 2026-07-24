528bb46a3f2c777b112e300167322ea140b2d28b
LuiyTheNinja <jc46124@gmail.com>
2026-07-24 01:09:28 -0400

    Added game.data and art for Day14 Project

--- Day 14/Higher_Or_Lower.py
+++ Day 14/Higher_Or_Lower.py
@@ -6,4 +6,10 @@
 # For our program, we will instead do, who has more followers on Instagram for this Project. 
 # Neymar a football player from Brazil or Khloe Kardashian, a reality TV Star. 
 # Rhianna or Shakira - Both Musicians
-# 
+
+# Intended Approach
+# 1. Breakdown the Problem
+# 2. Start with the Easiest task we get from breaking down the problem
+# 3. Turn the Prolem into comments 
+# 4. Write Code (Beneath the related comments) --> Run Code --> Debug Code
+# 5. NExt Task | One task at a time, easiest to hardest. 
