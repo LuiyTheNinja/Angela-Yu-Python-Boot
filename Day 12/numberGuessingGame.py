34a74189119465f94584be91ea10919ca986846a
LuiyTheNinja <jc46124@gmail.com>
2026-07-24 00:39:54 -0400

    Completed Day12 Number Guess Project

--- Day 12/numberGuessingGame.py
+++ Day 12/numberGuessingGame.py
@@ -1,22 +1,36 @@
 import random
 
-number_list = list(range(101))
+secret_number = random.randint(1, 100)
 
-secret_number = random.choice(number_list)
+print("Welcome to the Number Guessing Game!")
+print("I am thinking of a number between 1 and 100.")
 
-print("Welcome to the number guessing game!")
-print("I am thinking of a number between 1 and 100")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
-guess = input("Make a guess!: ")
-
-while guess != secret_number:
-  if difficulty == 'easy':
-    print("You have 10 attempts remaining to guess the number.")
+# Easy should give more attempts than hard
+if difficulty == 'easy':
     attempts = 10
-    guess = input("Make a guess!")
-if difficulty == 'hard':
-    print("You have 4 attemps remaining to guess the number.")
-    attempts = 4
-    guess = input("Make a guess!")
+else:
+    attempts = 5
 
+while attempts > 0:
+    print(f"\nYou have {attempts} attempt(s) remaining to guess the number.")
+    
+    try:
+        guess = int(input("Make a guess: "))
+    except ValueError:
+        print("Please enter a valid integer.")
+        continue
+
+    if guess == secret_number:
+        print(f" You got it! The answer was {secret_number}.")
+        break
+    elif guess > secret_number:
+        print("Too high!")
+        attempts -= 1
+    else:
+        print("Too low!")
+        attempts -= 1
+
+if attempts == 0:
+    print(f"\nYou've run out of guesses! The correct number was {secret_number}.")
